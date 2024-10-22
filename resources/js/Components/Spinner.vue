d3c5cbce21b8694fd7a213e1087cd6e24127ef99
Ely Errico <e.errico@socialwebsolutions.it>
2024-10-22 10:48:18 +0200

    Initial commit

---
+++ resources/js/Components/Spinner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="loading" class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  <div v-else>
+    <!-- Contenido de la página una vez que ha cargado -->
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  mounted() {
+    // Simulación de carga. Aquí podrías hacer la llamada a la API o algo similar.
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000); // Cambia el tiempo según tu necesidad
+  },
+};
+</script>
+
+<style scoped>
+/* Puedes agregar estilos personalizados si es necesario */
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Edu+TAS+Beginner:wght@400..700&family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+        <!-- Icon Font Stylesheet -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Libraries Stylesheet -->
+        {{-- <link rel="stylesheet" href="lib/animate/animate.min.css"/>
+        <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet"> --}}
+
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
+    </head>
+    <body class="">
+        @inertia
+    </body>
+</html>
