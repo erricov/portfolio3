40ed9ab195d373b03ed661c86fe4bfe4bc551f42
Ely Errico <e.errico@socialwebsolutions.it>
2024-10-26 11:40:26 +0200

    fix backend

--- resources/backend/js/Pages/Profile/Partials/DeleteUserForm.vue
+++ resources/backend/js/Pages/Profile/Partials/DeleteUserForm.vue
@@ -1,10 +1,10 @@
 <script setup>
-import DangerButton from '@/Components/DangerButton.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import Modal from '@/Components/Modal.vue';
-import SecondaryButton from '@/Components/SecondaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
+import DangerButton from '@backend/Components/DangerButton.vue';
+import InputError from '@backend/Components/InputError.vue';
+import InputLabel from '@backend/Components/InputLabel.vue';
+import Modal from '@backend/Components/Modal.vue';
+import SecondaryButton from '@backend/Components/SecondaryButton.vue';
+import TextInput from '@backend/Components/TextInput.vue';
 import { useForm } from '@inertiajs/vue3';
 import { nextTick, ref } from 'vue';
 
