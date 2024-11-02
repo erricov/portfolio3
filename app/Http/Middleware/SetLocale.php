9de97421b3319e3c767b72450e6fc9c402cd93a0
Ely Errico <e.errico@socialwebsolutions.it>
2024-11-02 10:39:00 +0100

    multilanguage

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -11,6 +11,7 @@ class SetLocale
     public function handle($request, Closure $next)
     {
         $locale = Session::get('locale', config('app.locale'));
+        // dd($locale);
         App::setLocale($locale);
 
         return $next($request);
